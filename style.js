bc7a67106d35c5c781c3d0752fdf836272088baa
copycatcodex <gaarandongdotcom@gmail.com>
2024-09-08 09:10:46 +0700

    Fix Code!

--- style.js
+++ style.js
@@ -1,66 +1,48 @@
-/***
- *    888 888         888 888                     d8   ,e,                  
- *    888 888  ,e e,  888 888 Y8b Y888P  ,e e,   d88    "   e88'888  ,"Y88b 
- *    8888888 d88 88b 888 888  Y8b Y8P  d88 88b d88888 888 d888  '8 "8" 888 
- *    888 888 888   , 888 888   Y8b "   888   ,  888   888 Y888   , ,ee 888 
- *    888 888  "YeeP" 888 888    Y8P     "YeeP"  888   888  "88,e8' "88 888 
- *                                                                            
- *    Name: Hellvetica
- *    KeepConnected
- *    @copycatcodex                                                                      
- */
-gambar = new Image();
+const gambar = new Image();
 gambar.src = "2.png";
 
-king = document.getElementById('K')
-lagu = "yumcut.mp3"
-
-//pop_i = 0
+const king = document.getElementById('K');
+const lagu = "yumcut.mp3";
 
 function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
+    const name = `${cname}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
     return "";
 }
+
 function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    const expires = `expires=${d.toUTCString()}`;
+    document.cookie = `${cname}=${cvalue};${expires};path=/`;
 }
 
-pop_count = getCookie("pop_count")
-king.innerHTML = pop_count > 0 ? pop_count : "0"
+let pop_count = getCookie("pop_count");
+king.innerHTML = pop_count > 0 ? pop_count : "0";
 
-ks_func = function (){
-    king.id ="KS"
-    
-    //Lagunya disini!
-    audio = new Audio(lagu)
-    audio.play()
-    
-    
-    
-    //Berhitung!
-    pop_count++
-    king.innerHTML = pop_count
-    setCookie("pop_count",pop_count,365)
-} 
-s_func = () => setTimeout(()=>king.id ="K",25)
+const ks_func = () => {
+    king.id = "KS";
+    const audio = new Audio(lagu);
+    audio.play();
+    pop_count++;
+    king.innerHTML = pop_count;
+    setCookie("pop_count", pop_count, 365);
+};
 
+const s_func = () => setTimeout(() => king.id = "K", 25);
 
-king.addEventListener("pointerup", s_func)
-king.addEventListener("keyup", s_func)
-king.addEventListener("pointerdown", ks_func)
-king.addEventListener("keydown", ks_func)
+king.addEventListener("pointerup", s_func);
+king.addEventListener("keyup", s_func);
+king.addEventListener("pointerdown", ks_func);
+king.addEventListener("keydown", ks_func);
 king.addEventListener('contextmenu', event => event.preventDefault());
